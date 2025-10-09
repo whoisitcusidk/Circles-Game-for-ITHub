c8e92c4828cac5e4a9460cab72fff0083c5a5fe6
kryakikrya <sereb8595@gmail.com>
2025-10-09 06:32:22 +0300

    Deleting useless scripts

--- Assets/Scripts/World/ObjectDestroyer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ObjectDestroyer : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Destroy(collision.gameObject);
-    }
-}
